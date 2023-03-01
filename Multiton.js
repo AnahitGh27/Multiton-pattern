68b9de6f47aadd0c60b8898d7bf94942f6f63d82
Anahit Ghazaryan <27anahit.ghazaryan@gmail.com>
2023-03-01 20:57:02 +0400

    constructor added and some other changes

--- Multiton.js
+++ Multiton.js
@@ -1,22 +1,29 @@
 class Multiton 
 {
-    static id = 0;
-    static times = 3;
-    static object = [];
+    static #id = -1;
+    static #countOfInstances = 10;
+    static #objects = [];
+    static #isInternalConstructing = false;
 
-    static createobject() 
+    constructor() {
+        if (!Multiton.#isInternalConstructing) {
+            throw new TypeError("Multiton is not constructable");
+        }
+    }
+
+    static createObject() 
     {
-        Multiton.id++;
+        Multiton.#id++;
 
-        if (Multiton.id <= Multiton.times) {
-            Multiton.object.push(new Multiton());
-            return Multiton.object[Multiton.id - 1];
+        if (Multiton.#id < Multiton.#countOfInstances) {
+            Multiton.#isInternalConstructing = true;
+            Multiton.#objects.push(new Multiton());
+            Multiton.#isInternalConstructing = false;
+            return Multiton.#objects[Multiton.#id];
         } 
 
-        const timesCount = Math.floor((Multiton.id / Multiton.times));
-        let index = Multiton.id - (Multiton.times * timesCount);
-        if (index === 0) { index = Multiton.object.length - 1; }
-        else { index -= 1; }
-        return Multiton.object[index];
+        const times = Math.floor(Multiton.#id / Multiton.#countOfInstances);
+        const index = Multiton.#id - (Multiton.#countOfInstances * times);
+        return Multiton.#objects[index];
     }
 }
